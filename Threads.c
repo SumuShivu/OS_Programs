328fcd1ff6b3e95d3c07187b1fd2e9db5b56e85a
SumuShivu <sumushivu5504@gmail.com>
2026-02-25 12:34:18 +0530

    Second

--- Threads.c
+++ Threads.c
@@ -10,12 +10,23 @@ void* abc(void* args){
     return res;
 }
 
+int* pqr(void* args){
+  int a,b;
+  scanf("%d%d",&a,&b);
+  int* res=(int*)malloc(sizeof(int));
+ *res=a-b;
+    return res;
+}
 int main(){
-    pthread_t thread1;
+    pthread_t thread1,thread2;
  void* a=(int*)malloc(sizeof(int));
+  void* b=(int*)malloc(sizeof(int));
   pthread_create(&thread1,NULL,abc,NULL);
     pthread_join(thread1,&a);
-  printf("ans=%d\n",*(int*)a);
-  free(a);
+    pthread_create(&thread2,NULL,pqr,NULL);
+  
+     pthread_join(thread2,&b);
+  printf("ans1=%d\n ans2=%d\n",*(int*)a,*(int*)b);
+  free(a);free(b);
     return 0;
 }
